8e0ad684fd029936d900cdde3cfa2a58f2288fa8
overdev <overrimachi2001@gmail.com>
2025-09-18 11:29:18 -0500

    with proyects

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,8 +1,3 @@
-const API_URL = "https://smtp.maileroo.com/send";
-const API_KEY = "ca2fac0a812be354968adca8d935edc2dcb32ba3b5ba401847729a047e520410";
-const form = new FormData();
-form.append("from", "overdev-code@gmail.com");
-form.append("to", "overdev-code@gmail.com");
-form.append("subject", "Contact Form");
-form.append("html", "Hello, this is a test email");
-
+// Este archivo ya no es necesario
+// La funcionalidad de envío de correo ha sido removida por seguridad
+// Los usuarios ahora pueden contactar directamente a través de redes sociales
